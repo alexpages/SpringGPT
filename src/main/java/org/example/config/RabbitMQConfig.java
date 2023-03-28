358d7e99c2e07022f06a25c7abf796f744a8ecb1
Alex Pages <alexpagesandreu@gmail.com>
2023-03-28 11:21:20 +0800

    Modified structure of the project

--- src/main/java/org/example/config/RabbitMQConfig.java
+++ src/main/java/org/example/config/RabbitMQConfig.java
@@ -1,13 +1,17 @@
-package org.example;
+package org.example.config;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootConfiguration
 public class RabbitMQConfig {
 
     ConnectionFactory factory = new ConnectionFactory();
 
+    @Configuration
     public RabbitMQConfig() {
         String userName = "guest";
         String password = "guest";
